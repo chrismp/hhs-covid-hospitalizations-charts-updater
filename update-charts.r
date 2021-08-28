3c440ac5ca21f43120a0508a276c0533f979f336
Chris P <christopher.mark.persaud@gmail.com>
2021-08-28 16:32:31 -0400

    add chart updater

---
+++ update-charts.r
@@ -0,0 +1,54 @@
+source('process.r')
+# devtools::install_github("munichrocker/DatawRappr",ref="master")
+
+library(DatawRappr)
+
+print("Starting chart updater")
+
+# startdate <- gsub(
+#   pattern = " 0",
+#   replacement = ' ',
+#   x = format(
+#     x = max(fullwithifips$collection_week_formatted,na.rm=T),
+#     format = "%B %d"
+#   )
+# )
+# 
+# enddate <- gsub(
+#   pattern = " 0",
+#   replacement = ' ',
+#   x = format(
+#     x = max(fullwithifips$collection_week_formatted,na.rm=T)+6,
+#     format = "%B %d"
+#   )
+# )
+# 
+# year <- gsub(
+#   pattern = " 0",
+#   replacement = ' ',
+#   x = format(
+#     x = max(fullwithifips$collection_week_formatted,na.rm=T)+6,
+#     format = "%Y"
+#   )
+# )
+
+chartIDs <- c(
+  'mS15D' # pbc icu beds in use/free
+)
+
+apikey <- Sys.getenv("DATAWRAPPER_API")
+
+# for (id in chartIDs) {
+#   print(id)
+#   dw_edit_chart(
+#     chart_id = id,
+#     api_key = apikey,
+#     annotate = paste0("Updated ",updateDateFormat,'.')
+#   )
+#   print("Publishing chart")  
+#   dw_publish_chart(
+#     chart_id = id,
+#     api_key = apikey,
+#     return_urls = TRUE
+#   )  
+# }
